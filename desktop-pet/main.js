04140b483ad644d11c66f8a3327753c0311d13b9
lsbillups <lsbillups@users.noreply.github.com>
2026-07-10 13:32:30 +0800

    Smaller pet window: 170x210

--- desktop-pet/main.js
+++ desktop-pet/main.js
@@ -98,7 +98,7 @@ function buildTrayMenu() {
 function createPetWindow() {
   const config = loadConfig();
   petWindow = new BrowserWindow({
-    width: 240, height: 280,
+    width: 170, height: 210,
     transparent: true, frame: false, alwaysOnTop: true,
     hasShadow: false, resizable: false, skipTaskbar: true,
     opacity: config.opacity,
